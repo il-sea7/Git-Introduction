907649cf8d29ce5300b479faab65b45b5b192844
Kamohelo <576658@student.belgiumcampus.ac.za>
2021-02-25 21:02:23 +0200

    init

---
+++ GIT.java
@@ -0,0 +1,19 @@
+public class GIT 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("First Git operation");
+        //comment
+        //comment 2
+        System.out.println("Code to check whther a number is odd or even from 0 to 100");
+        for(int i=0;i<100;i++){
+            if(i%2==0){
+                System.out.println("Numer is even");
+            }
+            else{
+                System.out.println("Numer is odd");
+            }
+        }
+        //cinn
+    }
+}
